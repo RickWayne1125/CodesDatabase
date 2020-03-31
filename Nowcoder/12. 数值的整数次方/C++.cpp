46ccb25c4b83da7b1458ff0d1622f75dd77d0a9d
Ricky <1037457338@qq.com>
2020-04-01 00:10:39 +0800

    new solution

--- Nowcoder/12. 数值的整数次方/C++.cpp
+++ Nowcoder/12. 数值的整数次方/C++.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     double Power(double base, int exponent) {
+        if (base == 1 || base == 0) return base;
         double ans = 1;
         int flag = 1;
         if (exponent < 0) {
             flag = 0;
-            exponent = -exponent;
+            exponent = abs(exponent);
         }
-        for (int i = 0; i < exponent; i++) {
+        ans = pow(base, exponent / 2);
+        ans = ans * ans;
+        if (exponent % 2 == 1) {
             ans *= base;
         }
         if (!flag)   return 1 / ans;
