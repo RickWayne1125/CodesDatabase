6aa6744ef408006a55b9259e93b10e50117db11e
rickwayne1125 <1037457338@qq.com>
2020-10-03 15:13:46 +0800

    ADD count.py to count word frequency

---
+++ NLP/homework1/count.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+dict = {}
+with open("data.txt", "r", encoding="utf-8") as f:
+    for line in f.readlines():
+        line = line.strip('\n')
+        # 每一行以双空格作为定界符进行拆分
+        list = line.split(sep="  ")
+        for item in list:
+            if (dict.__contains__(item)):
+                dict[item] += 1;
+            else:
+                dict.setdefault(item, 1)
+        print(line)
+print(dict)
