83e9b44cbccaf02947f4a346d91cefb66bd958be
rickwayne1125 <1037457338@qq.com>
2020-10-30 17:52:13 +0800

    ADD New BiPatch Functions

--- NLP/homework2/split.py
+++ NLP/homework2/split.py
@@ -6,9 +6,8 @@ def readData():
             info = l.split(l[1])
             word = info[1].split('\n')[0]
             dict.setdefault(word, int(info[0]))
-        print(dict)
+        # print(dict)
         return dict
-        # print(lines)
 
 
 def matchForward(sentence, dict):
@@ -55,33 +54,56 @@ def matchBi(result1, result2):
         return result2
     elif (len1 < len2):
         return result1
-    n=1
-    while(n<=max(len1,len2)):
-        cnt1=countWord(n,result1)
-        cnt2=countWord(n,result2)
-        if(cnt1<cnt2):
+    n = 1
+    while (n <= max(len1, len2)):
+        cnt1 = countWord(n, result1)
+        cnt2 = countWord(n, result2)
+        if (cnt1 < cnt2):
             return result1
-        elif(cnt1>cnt2):
+        elif (cnt1 > cnt2):
             return result2
         else:
-            if(cnt1==0):
+            if (cnt1 == 0):
                 return result1
             else:
-                n+=1
+                n += 1
 
-def countWord(n,result):
-    cnt=0
+
+def countWord(n, result):
+    cnt = 0
     for word in result:
-        if n==len(word):
-            cnt+=1
+        if n == len(word):
+            cnt += 1
     return cnt
+
+
+def matchBi2(result1, result2, dict):
+    cnt1 = 1
+    cnt2 = 1
+    for word in result1:
+        if (word in dict):
+            cnt1 *= dict[word]
+        else:
+            cnt1 *= 1
+    for word in result2:
+        if (word in dict):
+            cnt2 *= dict[word]
+        else:
+            cnt2 *= 1
+    if (cnt1 > cnt2):
+        return result1
+    else:
+        return result2
+
 
 if __name__ == "__main__":
     dict = readData()
     sentence = input("Please enter the test sentence: ")
     result1 = matchForward(sentence, dict)
-    print("The result of FMM: ",result1)
+    print("The result of FMM: ", result1)
     result2 = matchBack(sentence, dict)
-    print("The result of BMM: ",result2)
-    result3=matchBi(result1,result2)
-    print("The result of Bi-Direction: ",result3)
+    print("The result of BMM: ", result2)
+    result3 = matchBi(result1, result2)
+    print("The result of Bi-Direction: ", result3)
+    result4 = matchBi2(result1, result2, dict)
+    print("The result of New Bi-Direction: ", result4)
