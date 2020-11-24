8dac942ef7de3e5a40ba2206d908cd1e7c4d0e65
Ricky <1037457338@qq.com>
2020-11-24 15:21:24 +0800

    FIX Bugs in MultiLogistic

--- ML/project1/multiLogistic.py
+++ ML/project1/multiLogistic.py
@@ -28,21 +28,24 @@ class MultiLogistic:
         self.num = len(self.yt)  # 数据条数
         self.xt = np.column_stack((self.X, np.ones((self.num, 1))))
         print('\nTraining Iris-setosa OvR Learner: ')
-        # self.trainModel(0)
+        self.trainModel(0,8000)
 
         # Second learner
         self.X, self.yt = self.readData('Iris-versicolor')
         self.num = len(self.yt)  # 数据条数
         self.xt = np.column_stack((self.X, np.ones((self.num, 1))))
         print('\nTraining Iris-versicolor OvR Learner: ')
-        # self.trainModel(1)
+        self.trainModel(1,20000)
 
         # Third learner
         self.X, self.yt = self.readData('Iris-virginica')
         self.num = len(self.yt)  # 数据条数
         self.xt = np.column_stack((self.X, np.ones((self.num, 1))))
         print('\nTraining Iris-virginica OvR Learner: ')
-        # self.trainModel(2)
+        self.trainModel(2,20000)
+
+        # Show Beta
+        print(self.beta)
 
         # Test Data
         self.preData()
@@ -69,14 +72,12 @@ class MultiLogistic:
         n=int(2*self.raw_data1.shape[0]/3)
         self.train_data1=self.raw_data1[:n,:]
         self.test_data1=self.raw_data1[n:,:]
-        n = 2 * self.raw_data2.shape[0]
+        n = int(2*self.raw_data2.shape[0]/3)
         self.train_data2 = self.raw_data2[:n, :]
         self.test_data2 = self.raw_data2[n:, :]
-        n = 2 * self.raw_data3.shape[0]
+        n = int(2*self.raw_data3.shape[0]/3)
         self.train_data3 = self.raw_data3[:n, :]
         self.test_data3 = self.raw_data3[n:, :]
-        print(self.test_data1.shape[0])
-        print(self.test_data1)
 
     def readData(self, name):
         if name == 'Iris-setosa':
@@ -88,8 +89,12 @@ class MultiLogistic:
         elif name == 'Iris-virginica':
             raw1 = self.train_data3
             raw2 = np.row_stack((self.train_data1, self.train_data2))
+        print(raw1)
+        print(raw2)
         raw1[:, self.col] = 1
         raw2[:, self.col] = 0
+        print(raw1)
+        print(raw2)
         raw_data = np.row_stack((raw1, raw2))
         X = raw_data[:, :len(raw_data[0]) - 1]
         rawy = raw_data[:, len(raw_data[0]) - 1]
@@ -110,7 +115,7 @@ class MultiLogistic:
         dbeta = np.array(dbeta).reshape((self.col + 1, 1))
         self.beta[no] = self.beta[no] - self.eta * dbeta
 
-    def trainModel(self, no, epoch=3000, eta=0.001, eps=0.1):
+    def trainModel(self, no, epoch=10000, eta=0.001, eps=0.1):
         loss = []
         x = []
         self.eta = eta
@@ -118,7 +123,8 @@ class MultiLogistic:
             l = self.getLoss(no)
             loss.append(l)
             x.append(i)
-            print("epoch = ", i, ", loss = ", l)
+            if(epoch%100==0):
+                print("epoch = ", i, ", loss = ", l)
             if (l <= eps):
                 break;
             self.gradDescent(no)
@@ -126,21 +132,27 @@ class MultiLogistic:
         plt.show()
 
     def preData(self):
+        self.cnt=0
+        self.total=0
         print('\nPredicting Test Data:')
+        print('Predicting 1st Dataset:')
         for i in range(self.test_data1.shape[0]):
             testdata=self.test_data1[i]
             self.testData(i,testdata)
+        print('Predicting 2nd Dataset:')
         for i in range(self.test_data2.shape[0]):
-            testdata=self.test_data1[i]
+            testdata=self.test_data2[i]
             self.testData(i,testdata)
+        print('Predicting 3rd Dataset:')
         for i in range(self.test_data3.shape[0]):
-            testdata=self.test_data1[i]
+            testdata=self.test_data3[i]
             self.testData(i,testdata)
+        print('Total Accuracy: ',self.cnt/self.total)
 
     def testData(self,i,testdata):
         p = [0, 0, 0]
-        for no in range(2):
-            z = pow(np.e, np.dot(self.beta[no].T, np.column_stack((testdata[:4], np.ones(1)))))
+        for no in range(3):
+            z = pow(np.e, np.dot(self.beta[no].T, np.append(testdata[:4],1)))
             p[no] = z / (1 + z)
         if (p[0] >= p[1] and p[0] >= p[2]):
             pre = 'Iris-setosa'
@@ -151,7 +163,10 @@ class MultiLogistic:
         elif (p[2] >= p[0] and p[2] >= p[1]):
             pre = 'Iris-virginica'
             n = 2
-        print('pre = ', pre, ', real = ', testdata[4], ", posilibity = ", p[n])
+        print('pre = ', pre, ', real = ', testdata[4], ", posilibity = ", p)
+        self.total += 1
+        if pre==testdata[4]:
+            self.cnt+=1
 
 if __name__ == '__main__':
     model = MultiLogistic()
