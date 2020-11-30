9d47dfc964790c5543e5589a0b443af4b46b26be
rickwayne1125 <1037457338@qq.com>
2020-11-30 10:31:40 +0800

    ADD DecisionTree

--- ML/project2/DecisionTree.py
+++ ML/project2/DecisionTree.py
@@ -1,47 +1,62 @@
+import math
+
 import numpy as np
 import pandas as pd
+import collections
+from sklearn.datasets import load_iris
 
 
 class node:
-    def __init__(self):
-        self.value  # 分类参考值
-        self.left  # 左子节点
-        self.right  # 右子节点
-        self.dimension
+    def __init__(self, v=None, l=None, r=None, d=0, label=None, max=None, leaf=0, nh=0):
+        self.value = v  # 分类参考值
+        self.left = l  # 左子节点
+        self.right = r  # 右子节点
+        self.dimension = d  # 节点代表属性值在数据集中下标
+        self.leaf = leaf  # 是否为叶子节点
+        self.label = label  # 节点标签值
+        self.max = max  # 多数表决时使用的标签值
+        self.nh = nh  # 经验熵，用于剪枝
 
 
 class Model:
-    def __init__(self):
-        self.xr = []
-        self.yr = []
-        self.xtrain = []
-        self.ytrain = []
-        self.xtest = []
-        self.ytest = []
+    def __init__(self, x, y):
+        # self.xr = []
+        # self.yr = []
+        # self.xtrain = []
+        # self.ytrain = []
+        # self.xtest = []
+        # self.ytest = []
+        self.root = self.buildTree(x, y)
+
+    def getMost(self, y):
+        labels = collections.Counter(y).most_common(1)
+        return labels[0][0]
 
     def getEntropy(self, y):
         ent = 0
-        if y.size <= 1:
+        if y.size > 1:
+            cate = list(set(y))
+        else:
             cate = [y.item()]
             y = [y.item()]
-        else:
-            cate = list(set(y))
         for i in cate:
             temp = len([j for j in y if j == i]) / len(y)
-            ent -= temp * np.log(temp, 2)
+            ent -= temp * math.log(temp, 2)
         return ent
 
     def getGainEnt(self, x, y, d):
         gain = 0
         value = 0
         ent = self.getEntropy(y)
-        attr = sorted(list(set(x[:, d])))
+        attr = x[:, d]
+        attr = list(set(attr))
+        attr = sorted(attr)
         anum = len(attr)
         dnum = len(x[:, d])
         for i in range(anum - 1):
             tmpv = (attr[i] + attr[i + 1]) / 2
-            l = [j for j in range(dnum) if x[j, d] <= value]
-            r = [j for j in range(dnum) if x[j, d] > value]
+            l = [j for j in range(dnum) if x[j, d] <= tmpv]
+            r = [j for j in range(dnum) if x[j, d] > tmpv]
             yl = y[l]
             yr = y[r]
             tmpg = ent - (len(yl) / len(y)) * self.getEntropy(yl) - (len(yr) / len(y)) * self.getEntropy(yr)
@@ -49,3 +64,101 @@ class Model:
                 value = tmpv
                 gain = tmpg
         return gain, value
+
+    def getAttr(self, x, y):
+        gain = 0
+        value = 0
+        d = 0
+        for i in range(len(x[0])):
+            tmpg, tmpv = self.getGainEnt(x, y, i)
+            if tmpg > gain:
+                gain = tmpg
+                value = tmpv
+                d = i
+        return gain, value, d
+
+    def divideTree(self, x, y, v, d):
+        attr = x[:, d]
+        num = len(x[:, d])
+        l = [i for i in range(num) if x[i, d] <= v]
+        r = [i for i in range(num) if x[i, d] > v]
+        xl = x[l]
+        xr = x[r]
+        yl = y[l]
+        yr = y[r]
+        return xl, yl, xr, yr
+
+    def buildTree(self, x, y):
+        maxlabel = self.getMost(y)
+        ent = self.getEntropy(y)
+        nh = ent * len(y)
+        if y.size > 1:
+            gain, value, d = self.getAttr(x, y)
+            if gain > 0:
+                xl, yl, xr, yr = self.divideTree(x, y, value, d)
+                ltree = self.buildTree(xl, yl)
+                rtree = self.buildTree(xr, yr)
+                return node(v=value, l=ltree, r=rtree, d=d, max=maxlabel, nh=nh)
+            else:
+                return node(label=y[0], max=maxlabel, leaf=1, nh=nh)
+        else:
+            return node(label=y.item(), max=maxlabel, leaf=1, nh=nh)
+
+    def postPruning(self, root, alpha):
+        if root.leaf == 0:
+            if root.left.leaf == 0:
+                self.postPruning(root.left, alpha)
+            if root.right.leaf == 0:
+                self.postPruning(root.right, alpha)
+        if root.left.leaf == 1 and root.right.leaf == 1:
+            bef = (root.left.nh + alpha) + (root.right.nh + alpha)
+            aft = root.nh + alpha
+            if bef >= aft:
+                root.left = None
+                root.right = None
+                root.leaf = 1
+                root.label = root.max
+
+    def classify(self, input, root):
+        if root.label != None:
+            return root.label
+        else:
+            tmp = input[root.dimension]
+            if tmp <= root.value:
+                next = root.left
+            else:
+                next = root.right
+            return self.classify(input, next)
+
+
+if __name__ == '__main__':
+    data = load_iris()
+    x = data.data
+    y = data.target
+    permutation = np.random.permutation(x.shape[0])
+    shuffled_dataset = x[permutation, :]
+    shuffled_labels = y[permutation]
+    tlen = int(len(shuffled_labels) * 2 / 3)
+    print('Size of Test Datasets:', tlen)
+    dlen = len(shuffled_labels)
+    train_data = shuffled_dataset[:tlen, :]
+    train_label = shuffled_labels[:tlen]
+    test_data = shuffled_dataset[tlen:dlen, :]
+    test_label = shuffled_labels[tlen:dlen]
+    model = Model(train_data, train_label)
+    true_count = 0
+    for i in range(len(test_label)):
+        predict = model.classify(test_data[i], model.root)
+        print('Prediction: ', predict, 'Real: ', test_label[i])
+        if predict == test_label[i]:
+            true_count += 1
+    print("Before Pruning Accuracy: ", (true_count / len(test_data)))
+    true_count = 0
+    root = model.root
+    model.postPruning(root, alpha=4)
+    for i in range(len(test_label)):
+        predict = model.classify(test_data[i], root)
+        print('Prediction: ', predict, 'Real: ', test_label[i])
+        if predict == test_label[i]:
+            true_count += 1
+    print("After Pruning Accuracy: ", (true_count / len(test_data)))
