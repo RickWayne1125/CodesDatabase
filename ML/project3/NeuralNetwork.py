b553f525eb308e6b8615f5cdb43ff734742f76cb
rickwayne1125 <1037457338@qq.com>
2020-12-01 11:52:00 +0800

    ADD ActivFunct

--- ML/project3/NeuralNetwork.py
+++ ML/project3/NeuralNetwork.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 class Neuron:
@@ -7,8 +8,22 @@ class Neuron:
         self.bias = b
         self.act_method = active
         self.input_size = n
+        self.result = None
+        self.output = None
 
     def activeFunction(self):
         if self.act_method == 'Sigmoid':
+            self.output = 1 / (1 + pow(math.e, self.result))
+        elif self.act_method == 'ReLU':
+            self.output = max(0, self.result)
+        elif self.act_method == 'Tanh':
+            tmp = pow(math.e, self.result)
+            self.output = (tmp - 1 / tmp) / (tmp + 1 / tmp)
+        else:
+            print('Method Not Found!')
 
     def forwardCal(self, input):
+        self.input_size = len(input)
+        self.result = np.dot(input, self.weight) + self.bias
+
+    def getLoss(self):
