016a7ec0b1eac6f43cebb4110fb4de90897ff612
Ricky <1037457338@qq.com>
2020-12-12 13:34:48 +0800

    ADD Basic Functions

--- ML/project3/NeuralNetwork.py
+++ ML/project3/NeuralNetwork.py
@@ -8,12 +8,19 @@ class Neuron:
         self.bias = b
         self.act_method = active
         self.input_size = n
-        self.result = None
-        self.output = None
+        self.result = None  # 未激活前的输出值
+        self.output = None  # 激活后实际输出值
+        self.initParameters()
+
+    def initParameters(self):
+        self.weight = np.random.randn(self.input_size, 1)
+        self.b = np.zeros((1, 1))
+        print(self.weight)
+        print(self.b)
 
     def activeFunction(self):
         if self.act_method == 'Sigmoid':
-            self.output = 1 / (1 + pow(math.e, self.result))
+            self.output = 1 / (1 + np.exp(-self.result))
         elif self.act_method == 'ReLU':
             self.output = max(0, self.result)
         elif self.act_method == 'Tanh':
@@ -26,4 +33,14 @@ class Neuron:
         self.input_size = len(input)
         self.result = np.dot(input, self.weight) + self.bias
 
-    def getLoss(self):
+    # def getLoss(self):
+
+
+class BPNet:
+    def __init__(self, layer_list, active_list):
+        self.layer_num = len(layer_list)
+        self.layer_size = layer_list
+
+
+if __name__ == '__main__':
+    n = Neuron(n=10)
