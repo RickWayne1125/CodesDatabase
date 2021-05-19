b1b5825da322129dec2f99c38318d77b049d5c7a
ricky <1037457338@qq.com>
2021-05-19 11:34:12 +0800

    FINISH getFIRST function

--- Compiler/toy_compiler/SyntaxAnalysis.cpp
+++ Compiler/toy_compiler/SyntaxAnalysis.cpp
@@ -5,61 +5,113 @@
 
 using namespace std;
 
-
-class AST {
+class AST
+{
 private:
     string text; // 当前串
 public:
     vector<AST *> children; // 子节点
 };
 
-class LL1 {
+class LL1
+{
 private:
     vector<string> token_list;
     vector<node> elements;
     stack<node> ana_stack;
     map<string, set<string>> first_map;
     map<string, set<string>> follow_map;
-    map<string, vector<production>> pros;   // map<non terminal symbol on the left, production>
+    map<string, vector<production>> pros; // map<non terminal symbol on the left, production>
 public:
-    LL1(vector<string> tl, vector<node> e) {
+    LL1(vector<string> tl, vector<node> e)
+    {
         token_list = tl;
         elements = e;
+        initProduction;
     }
 
+    void insertFIRST(string left, string cur_symbol)
+    {
+        if (first_map[cur_symbol].size() == 0)
+        {
+            getFIRST(cur_symbol);
+        }
+        for (auto iter : first_map[cur_symbol])
+        {
+            first_map[left].insert(iter);
+        }
+    }
 
-    void getFIRST(string left) {
+    void getFIRST(string left)
+    {
         vector<production> pro = pros[left];
-        for (int i = 0; i < pro.size(); ++i) {
-            string cur_symbol = pro[i].right[0];    // 产生式右侧第一个符号
+        for (int i = 0; i < pro.size(); ++i)
+        {
+            string cur_symbol = pro[i].right[0]; // 产生式右侧第一个符号
             // 若该符号为终结符
-            if (TERMINAL_LIST.find(cur_symbol) != TERMINAL_LIST.end()) {
+            if (TERMINAL_LIST.find(cur_symbol) != TERMINAL_LIST.end())
+            {
                 first_map[left].insert(cur_symbol);
-            } else if (NON_TERMINAL_LIST.find(cur_symbol) != NON_TERMINAL_LIST.end()) {
-//                while(pros[cur_symbol])
-                if (first_map[cur_symbol].size() != 0) {
-                    for (auto iter:first_map[cur_symbol]) {
-                        first_map[left].insert(iter);
+            }
+            else if (NON_TERMINAL_LIST.find(cur_symbol) != NON_TERMINAL_LIST.end())
+            {
+                insertFIRST(left, cur_symbol);
+                for (size_t j = 0; j < pro[i].right.size(); j++)
+                {
+                    // 当前符号可以指向空时
+                    // Y1...Yj-1 -> none 则 FIRST(Yj)加入FIRST(X)
+                    int flag = 0;                           // 表示当前符号是否可以指向none
+                    for (auto iter : pros[pro[i].right[j]]) // production iter
+                    {
+                        if (iter.right[0] == "none")
+                        {
+                            if (j != pro[i].right.size() - 1)
+                                insertFIRST(left, pro[i].right[j + 1]);
+                            else // 对于产生式文末符号
+                            {
+                                first_map[left].insert("none"); // 将none加入FIRST(X)
+                            }
+                            flag = 1;
+                            break;
+                        }
                     }
-                } else {
-                    getFIRST(cur_symbol);
+                    if (flag == 0)
+                    {
+                        break; // 若当前符号不可指向none则不再继续判断
+                    }
                 }
             }
         }
-//        for (auto iter:pros) {
-//            for (int i = 0; i < iter.second.size(); i++) {
-//                // 产生式右侧第一个符号为终结符时
-//                if (TERMINAL_LIST.find(iter.second[i].right[0]) != TERMINAL_LIST.end()) {
-//                    first += iter.second[i].right[0];
-//                } else if (NON_TERMINAL_LIST.find(iter.second[i].right[0]) != NON_TERMINAL_LIST.end()) {
-//
-//                }
-//            }
-//        }
+    }
+
+    void initProduction()
+    {
+        for (auto iter : productions)
+        {
+            production p(iter);
+            if (pros.find(p.left) != pros.end())
+            { // map中已经存有key的情况
+                pros[p.left].push_back(p);
+            }
+            else
+            { // map中不存在key的情况
+                vector<production> temp;
+                temp.push_back(p);
+                pros.insert(pair<string, vector<production>>(p.left, temp));
+            }
+        }
+    }
+
+    void getFIRSTset()
+    {
+        for (auto iter : pros)
+        {
+        }
     }
 };
 
-int main() {
+int main()
+{
     production p(DECLARE_INT);
     p.show();
 }
--- Compiler/toy_compiler/productions.h
+++ Compiler/toy_compiler/productions.h
@@ -3,22 +3,30 @@
 //
 #include <bits/stdc++.h>
 // 终结符表与非终结符表
-set<string> NON_TERMINAL_LIST {"S","A","B","C"};
-set<string> TERMINAL_LIST={"if","+","id","-","*","/","int"};
+set<string> NON_TERMINAL_LIST{"S", "A", "B", "C"};
+set<string> TERMINAL_LIST = {"none", "if", "+", "id", "-", "*", "/", "int"};
 
-// 产生式列表
+// 产生式编码
 #define DECLARE_INT "S -> int A"
 
+// 产生式列表
+vector<string> productions = {
+    DECLARE_INT
+};
+
 // 分割函数
-vector<string> split(const string &str, const string &delim) {
+vector<string> split(const string &str, const string &delim)
+{
     vector<string> res;
-    if ("" == str) return res;
+    if ("" == str)
+        return res;
     char *strs = new char[str.length() + 1];
     strcpy(strs, str.c_str());
     char *d = new char[delim.length() + 1];
     strcpy(d, delim.c_str());
     char *p = strtok(strs, d);
-    while (p) {
+    while (p)
+    {
         string s = p;
         res.push_back(s);
         p = strtok(NULL, d);
@@ -27,24 +35,28 @@ vector<string> split(const string &str, const string &delim) {
 }
 
 // 产生式类型
-struct production {
-    string left;    // 产生式左侧
-    vector<string> right;   // 产生式右侧
-    production(string pro) {
+struct production
+{
+    string left;          // 产生式左侧
+    vector<string> right; // 产生式右侧
+    production(string pro)
+    {
         vector<string> sp_r = split(pro, " ");
         left = sp_r[0];
-        for (int i = 2; i < sp_r.size(); i++) {
+        for (int i = 2; i < sp_r.size(); i++)
+        {
             right.push_back(sp_r[i]);
         }
     }
 
-    void show() {
+    void show()
+    {
         std::cout << "Production: ";
         std::cout << left << " -> ";
-        for (int i = 0; i < right.size(); i++) {
+        for (int i = 0; i < right.size(); i++)
+        {
             std::cout << right[i] << ' ';
         }
         std::cout << endl;
     }
 };
-
